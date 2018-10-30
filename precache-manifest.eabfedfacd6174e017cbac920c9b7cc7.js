06aea1a0950f4aeb260ce843398f541fb8f63763
gloryarz <gloryarz@gmail.com>
2018-10-30 01:35:08 -0600

    Updates

---
+++ precache-manifest.eabfedfacd6174e017cbac920c9b7cc7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b7077a52fc3959369606",
+    "url": "/hackaton/static/css/main.b3e99a1d.chunk.css"
+  },
+  {
+    "revision": "b7077a52fc3959369606",
+    "url": "/hackaton/static/js/main.b7077a52.chunk.js"
+  },
+  {
+    "revision": "9e8a5d0e3036ba1a61f7",
+    "url": "/hackaton/static/js/1.9e8a5d0e.chunk.js"
+  },
+  {
+    "revision": "c60f846137f325237aa5",
+    "url": "/hackaton/static/js/runtime~main.c60f8461.js"
+  },
+  {
+    "revision": "b009f6ba1e4343b1156b496596ac6fed",
+    "url": "/hackaton/index.html"
+  }
+];
