f9b6b635cdda8fce8e77ba56803545c603532d11
gloryarz <gloryarz@gmail.com>
2018-10-30 04:34:57 -0600

    Estilos matrñicla

--- src/Components/Matricula/Matricula.jsx
+++ src/Components/Matricula/Matricula.jsx
@@ -1,15 +1,27 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { Col, Button, Row } from "react-bootstrap";
+import '../CSS/main.css'
+import './matricula.css'
+
 
 const Matricula = () => {
   return (
     <div>
-      <p>
-        Matricula
-        <button>
+      <Row>
+        <Col xs={12} className="color">
+          <h4 className="center head">Crea tu cuenta</h4>
+        </Col>
+        <Col xs={10} xsOffset={1} className="txt">
+        <p >Número de matrícula de tu hijo</p>
+        <input type="text"/> <br/>
+
+        <Button className="btn color marginBtn">
           <NavLink to="/verificacion">Boton</NavLink>
-        </button>
-      </p>
+        </Button>
+      </Col>
+      </Row>
+      
     </div>
   );
 };
